99ba804fff4f78fd22257ecefcd90423fdeb0a4a
janicewei0827 <101675178+janicewei0827@users.noreply.github.com>
2022-05-06 14:29:13 +1200

    04_prize_generation_v1

---
+++ 04_prize_generation_v1.py
@@ -0,0 +1,9 @@
+import random
+
+NUM_TRIALS = 10
+
+for item in range(0, NUM_TRIALS):
+
+ #randiant finds numbers between given end points including both endpoints
+    prize_num=random.randint(1,4)
+    print(prize_num)
